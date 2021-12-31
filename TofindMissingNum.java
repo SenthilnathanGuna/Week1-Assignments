41d7b00f78e46e66fb848af8be268e6820cbaea3
SenthilnathanGuna <96657279+SenthilnathanGuna@users.noreply.github.com>
2021-12-31 20:08:39 +0000

    Add files via upload

--- TofindMissingNum.java
+++ TofindMissingNum.java
@@ -1,19 +1,48 @@
 package week1.day2;
 
+import java.util.Arrays;
+
 public class TofindMissingNum 
 {
 
 	public int tofindMissingNum(int test[])
 	{
-		int i = 0;
+		int i=1,j;
 		
-		return i;
+		Arrays.sort(test);
+		
+		for(j=0;j<test.length;j++)
+		{
+			
+		//	System.out.println("value : " + test[j]);
+			if(i==test[j])
+			{
+			//	System.out.println("Matched"+i);
+				i++;
+				
+			}
+			else
+			{
+				System.out.println("Missing number in Array : "+i);
+				i=i+2;
+								
+			}
+			
+		/*	else
+			{
+				System.out.println("Missing element in Array :"+i);
+			}
+			 */
+		}
+		
+		return 1;
 	}
 	
 	public static void main(String[] args) 
 	{
 		TofindMissingNum obj2 = new TofindMissingNum();
-	//	obj2.tofindMissingNum(int test[1,2]);
+		int testarray1 [] = {3,1,2,4,6,7,8,10};
+		obj2.tofindMissingNum(testarray1);
 
 	}
 
